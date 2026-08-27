8c29a578d72f7af5142250f2d676c949f927e5d8
varadkachave <varadkachave203@gmail.com>
2026-08-27 22:01:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/varad/in/spring_boot_jwt_authentication_api/SpringBootJwtAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package varad.in.spring_boot_jwt_authentication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/varad/in/spring_boot_jwt_authentication_api/SpringBootJwtAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package varad.in.spring_boot_jwt_authentication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
